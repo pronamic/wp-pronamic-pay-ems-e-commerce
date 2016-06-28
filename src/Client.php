79125ef527dc53332b8340432c666f29bfe258eb
I <info@remcotolsma.nl>
2016-06-28 14:52:06 +0200

    Removed unused language constants and helper functions.

--- src/Client.php
+++ src/Client.php
@@ -31,39 +31,6 @@ class Pronamic_WP_Pay_Gateways_EMS_ECommerce_Client {
 
 	//////////////////////////////////////////////////
 
-	const ISO_639_1_ENGLISH = 'en';
-
-	const ISO_639_1_FRENCH = 'fr';
-
-	const ISO_639_1_GERMAN = 'de';
-
-	const ISO_639_1_ITALIAN = 'it';
-
-	const ISO_639_1_SPANISH = 'es';
-
-	const ISO_639_1_DUTCH = 'nl';
-
-	//////////////////////////////////////////////////
-
-	public static function get_supported_language_codes() {
-		return array(
-			self::ISO_639_1_ENGLISH,
-			self::ISO_639_1_FRENCH,
-			self::ISO_639_1_GERMAN,
-			self::ISO_639_1_ITALIAN,
-			self::ISO_639_1_SPANISH,
-			self::ISO_639_1_DUTCH,
-		);
-	}
-
-	public static function is_supported_language( $language ) {
-		$languages = self::get_supported_language_codes();
-
-		return in_array( $language, $languages, true );
-	}
-
-	//////////////////////////////////////////////////
-
 	/**
 	 * Hash algorithm SHA256 indicator
 	 *
