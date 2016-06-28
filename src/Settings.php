5b5b4248515c9ec0cb961ead5cc7cd2e8fb9cb39
I <info@remcotolsma.nl>
2016-06-28 14:21:06 +0200

    Renamed 'Shared secret' to 'Shared Secret' exactly like in EMS e-mail.

--- src/Settings.php
+++ src/Settings.php
@@ -50,7 +50,7 @@ class Pronamic_WP_Pay_Gateways_EMS_ECommerce_Settings extends Pronamic_WP_Pay_Ga
 			'filter'      => FILTER_SANITIZE_STRING,
 			'section'     => 'ems_ecommerce',
 			'meta_key'    => '_pronamic_gateway_ems_ecommerce_secret',
-			'title'       => __( 'Shared secret', 'pronamic_ideal' ),
+			'title'       => __( 'Shared Secret', 'pronamic_ideal' ),
 			'type'        => 'text',
 			'classes'     => array( 'large-text', 'code' ),
 		);
