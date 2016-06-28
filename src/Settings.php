e72922ade11cf98533177b08963be8013666ba35
I <info@remcotolsma.nl>
2016-06-28 14:21:54 +0200

    Added context so this is translated correctly.

--- src/Settings.php
+++ src/Settings.php
@@ -50,7 +50,7 @@ class Pronamic_WP_Pay_Gateways_EMS_ECommerce_Settings extends Pronamic_WP_Pay_Ga
 			'filter'      => FILTER_SANITIZE_STRING,
 			'section'     => 'ems_ecommerce',
 			'meta_key'    => '_pronamic_gateway_ems_ecommerce_secret',
-			'title'       => __( 'Shared Secret', 'pronamic_ideal' ),
+			'title'       => _x( 'Shared Secret', 'ems', 'pronamic_ideal' ),
 			'type'        => 'text',
 			'classes'     => array( 'large-text', 'code' ),
 		);
