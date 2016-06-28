25d747d0f15521af2d7f4defbdf500f7eda8b55e
I <info@remcotolsma.nl>
2016-06-28 14:10:46 +0200

    Fixing PHPUnit test.

--- tests/StatusesTest.php
+++ tests/StatusesTest.php
@@ -10,21 +10,21 @@
  * @version 1.0.0
  * @since 1.0.0
  */
-class Pronamic_WP_Pay_EMS_StatusesTest extends PHPUnit_Framework_TestCase {
+class Pronamic_WP_Pay_Gateways_EMS_StatusesTest extends PHPUnit_Framework_TestCase {
 	/**
 	 * Test transform.
 	 *
 	 * @dataProvider status_matrix_provider
 	 */
 	public function test_transform( $mollie_status, $expected ) {
-		$status = Pronamic_WP_Pay_EMS_Statuses::transform( $mollie_status );
+		$status = Pronamic_WP_Pay_Gateways_EMS_Statuses::transform( $mollie_status );
 
 		$this->assertEquals( $expected, $status );
 	}
 
 	public function status_matrix_provider() {
 		return array(
-			array( Pronamic_WP_Pay_EMS_Statuses::OPEN, Pronamic_WP_Pay_Statuses::OPEN ),
+			array( Pronamic_WP_Pay_Gateways_EMS_Statuses::OPEN, Pronamic_WP_Pay_Statuses::OPEN ),
 			array( 'not existing status', null ),
 		);
 	}
