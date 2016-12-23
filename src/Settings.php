02a9e9919fce66768c41fd1a5c99290ff01ff5c1
Remco Tolsma <info@remcotolsma.nl>
2016-12-23 09:55:38 +0100

    Make sure to not encode quotes, otherwise single quotes `’` will result in a `&#39;`.

--- src/Settings.php
+++ src/Settings.php
@@ -47,7 +47,10 @@ class Pronamic_WP_Pay_Gateways_EMS_ECommerce_Settings extends Pronamic_WP_Pay_Ga
 
 		// Shared secret
 		$fields[] = array(
-			'filter'      => FILTER_SANITIZE_STRING,
+			'filter'      => array(
+				'filter' => FILTER_SANITIZE_STRING,
+				'flags'  => FILTER_FLAG_NO_ENCODE_QUOTES,
+			),
 			'section'     => 'ems_ecommerce',
 			'meta_key'    => '_pronamic_gateway_ems_ecommerce_secret',
 			'title'       => _x( 'Shared Secret', 'ems', 'pronamic_ideal' ),
