e840dddfa2cbcd9a8cafc4736c2770539a0a795d
Reüel van der Steege <reuel@pronamic.nl>
2019-12-22 22:02:52 +0100

    Update version 2.0.4.

--- src/Gateway.php
+++ src/Gateway.php
@@ -14,7 +14,7 @@ use Pronamic\WordPress\Pay\Payments\Payment;
  * Company: Pronamic
  *
  * @author Reüel van der Steege
- * @version 2.0.1
+ * @version 2.0.4
  * @since 1.0.0
  */
 class Gateway extends Core_Gateway {
