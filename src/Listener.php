935ec3a2d90bf605d0db2a6657f77b71ad458320
Reüel van der Steege <reuel@pronamic.nl>
2020-01-02 14:31:54 +0100

    Happy 2020.

--- src/Listener.php
+++ src/Listener.php
@@ -7,7 +7,7 @@ use Pronamic\WordPress\Pay\Plugin;
 /**
  * Title: EMS e-Commerce listener
  * Description:
- * Copyright: 2005-2019 Pronamic
+ * Copyright: 2005-2020 Pronamic
  * Company: Pronamic
  *
  * @author Reüel van der Steege
--- tests/src/PaymentMethodsTest.php
+++ tests/src/PaymentMethodsTest.php
@@ -7,7 +7,7 @@ use Pronamic\WordPress\Pay\Core\PaymentMethods as Core_PaymentMethods;
 /**
  * Title: EMS e-Commerce Gateway payment methods tests
  * Description:
- * Copyright: 2005-2019 Pronamic
+ * Copyright: 2005-2020 Pronamic
  * Company: Pronamic
  *
  * @author Remco Tolsma
